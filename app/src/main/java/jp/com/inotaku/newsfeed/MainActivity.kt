a55f9c34f3c2b61ea359b0a2fc88ec569d8bba9b
inotakuya <i_no_u_u@yahoo.co.jp>
2020-07-12 11:53:05 +0900

    fragmentに遷移するように修正

--- app/src/main/java/jp/com/inotaku/newsfeed/MainActivity.kt
+++ app/src/main/java/jp/com/inotaku/newsfeed/MainActivity.kt
@@ -3,11 +3,15 @@ package jp.com.inotaku.newsfeed
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import jp.com.inotaku.R
+import jp.com.inotaku.newsfeed.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.flagment_container, MainFragment())
+            .commit()
     }
 }
