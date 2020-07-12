cc0d052201a6734cb9048c9537d5c3821b7245bf
inotakuya <i_no_u_u@yahoo.co.jp>
2020-07-12 11:26:20 +0900

    Launcher Screen作成

--- app/src/main/java/jp/com/inotaku/newsfeed/MainActivity.kt
+++ app/src/main/java/jp/com/inotaku/newsfeed/MainActivity.kt
@@ -1,10 +1,12 @@
-package jp.com.inotaku
+package jp.com.inotaku.newsfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.com.inotaku.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
